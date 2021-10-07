29af1f35f0d2f71a8b5824b8a6091d553f4e011b
Saltcult <qubakuba@gmail.com>
2021-10-07 11:31:05 +0000

    save2

---
+++ src/components/exchangebox.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM, { render }  from "react-dom";
+
+class exchangebox extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+}
+
+render() {
+    return(
+     <>
+           <p>{this.props.currencyName.toUpperCase()}</p>
+        <input
+          type="text"
+          value={this.props.currencyValue}
+          onChange={(event) => {
+            this.props.changeCurrency(
+              event.target.value,
+              this.props.currencyName
+            );
+          }}
+        />
+      </>
+    );
+  }
+}
+
+export default exchangebox;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM  from "react-dom";
-import exchangebox from "./components/exchangebox"
+import exchangebox from "/workspace/thermometer/src/components/exchangebox"
 
 class exchangebox extends React.Component {
     constructor(props) {
@@ -10,7 +10,7 @@ class exchangebox extends React.Component {
         PLN: 0;
         USD: 0;
         }
-        
+
         this.usdtopln = this.usdtopln.bind(this);
     }
 
@@ -24,4 +24,23 @@ class exchangebox extends React.Component {
         }
     }
 };
+render() {
+    return (
+      <main>
+        <exchangebox
+          currencyName="USD"
+          currencyValue={this.state.USD}
+          usdtopln={this.usdtopln}
+        />
 
+        <exchangebox
+          currencyName="PLN"
+          currencyValue={this.state.PLN}
+          usdtopln={this.usdtopln}
+        />
+      </main>
+    );
+  }
+}
+
+ReactDOM.render(<Converter />, document.getElementById("root"));
